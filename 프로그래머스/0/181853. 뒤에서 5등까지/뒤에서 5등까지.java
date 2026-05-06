bd1a6193256a8776faf0f6f2762b6546daf5a3ee
20233017-RHW <145472464+20233017-RHW@users.noreply.github.com>
2026-05-07 01:57:07 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.58 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -0,0 +1,15 @@
+import java.util.PriorityQueue;
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[5];
+            PriorityQueue<Integer> pq = new PriorityQueue<>();
+            for(int i = 0; i < num_list.length; i++) {
+                pq.add(num_list[i]);
+            }
+            for(int i = 0; i < 5; i++) {
+                answer[i] = pq.peek();
+                pq.poll();
+            }
+            return answer;
+    }
+}
